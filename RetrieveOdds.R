ca8b596482840f115d53027cb7b808cc45d48e6e
KitoSTheron <145708071+KitoSTheron@users.noreply.github.com>
2024-10-13 11:43:47 +0100

    Formatted API bet odds

--- RetrieveOdds.R
+++ RetrieveOdds.R
@@ -3,6 +3,9 @@ library(httr)
 library(jsonlite)
 library(shiny)
 library(shinythemes)
+library(gamlss)
+library(DBI)
+library(RSQLite)
 # Function to fetch odds from the Odds API
 get_odds <- function(api_key, sport, region = "us", market = "h2h") {
   base_url <- "https://api.the-odds-api.com/v4/sports/"
@@ -29,12 +32,36 @@ get_odds <- function(api_key, sport, region = "us", market = "h2h") {
 # Example usage
 api_key <- Sys.getenv("MY_API_KEY")
 sport <- "basketball_nba" 
-print(api_key)
 
 # Fetch and print the odds
 odds <- get_odds(api_key, sport)
-print(odds)
+#print(odds)
 
+if ("bookmakers" %in% names(odds)) {
+  # Access and print bookmakers' odds for this event
+  for (bookmaker in odds$bookmakers) {
+    
+    # Loop through each market for the bookmaker
+    for (i in seq_along(bookmaker$markets)) {
+      market <- bookmaker$markets[[i]]  # Access each market
+      title <- bookmaker$title[i] 
+      cat("Bookmaker: ",title, "\n")
+      cat("Date:", market$last_update, "\n")
+      
+      # Loop through each outcome within the market
+      for (outcome in market$outcomes) {
+        cat("Team/Outcome:", outcome$name, "\n")
+        cat("Odds:", outcome$price, "\n\n")
+      }
+    }
+    cat("\n")
+  }
+} else {
+  cat("No bookmakers found in the first event.\n")
+}
+
+
+your_data <- dbReadTable(conn, "odds")
 # Define UI
 ui <- fluidPage(
   theme = shinytheme("cerulean"), # Use the Cerulean theme
