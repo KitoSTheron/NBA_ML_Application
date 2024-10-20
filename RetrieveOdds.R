b2b470d13b6ebf2753598c953ff1c09f9be5091d
KitoSTheron <145708071+KitoSTheron@users.noreply.github.com>
2024-10-20 16:46:25 +0100

    Colour codes good bets based on games from the previous season

--- RetrieveOdds.R
+++ RetrieveOdds.R
@@ -7,34 +7,72 @@ library(gamlss)
 library(DBI)
 library(RSQLite)
 library(dplyr)
+library(readxl)
+library(crayon)
 
 
 
-nba_data <- read.csv("TrainingData.csv")
-nba_data <- nba_data %>%
-  filter(season_id %in% c(12022,22022,32022,42022))
-nba_data <- nba_data %>%
-  mutate(wl_home = ifelse(wl_home == "W", 1, 0))
-nba_data <- nba_data %>%
-  select(wl_home, team_name_home, team_name_away)
 
+# this dataset is for the 2023-2024 season
+Last_NBA_SEASON <- read_excel("Last_NBA_SEASON.xlsx")
+Last_NBA_SEASON <-  Last_NBA_SEASON %>%
+  filter(rowSums(is.na(.)) != ncol(.))
 
+Last_NBA_SEASON <- Last_NBA_SEASON %>%
+  rename(Visitor = `Visitor/Neutral`, 
+         Home = `Home/Neutral`, 
+         VisitorPoints = `PTS...4`, 
+         HomePoints = `PTS...6`)
+
+Last_NBA_SEASON <-  Last_NBA_SEASON %>%
+  filter(rowSums(is.na(.)) != ncol(.))
+Last_NBA_SEASON <- Last_NBA_SEASON %>%
+  select(Visitor, Home, VisitorPoints,HomePoints)
+Last_NBA_SEASON$HomeScoreDif <- Last_NBA_SEASON$HomePoints - Last_NBA_SEASON$VisitorPoints
 
 gamlss_model <- gamlss(
-  wl_home ~ team_name_home + team_name_away,
+  (HomeScoreDif>0) ~ Home + Visitor,
   family = BI,  # Binomial distribution
-  data = nba_data
+  data = Last_NBA_SEASON
 )
 
-
 new_game <- data.frame(
-  team_name_home = "Boston Celtics",
-  team_name_away = "Denver Nuggets"
+  Home = "Denver Nuggets",
+  Visitor = "Brooklyn Nets"
 )
 
 predicted_prob <- predict(gamlss_model, newdata = new_game, type = "response")
 
 predicted_prob
+
+# this dataset is the last 20 years not including last year
+#nba_data <- read.csv("TrainingData.csv")
+#nba_data <- nba_data %>%
+#filter for 2022 season
+#  filter(season_id %in% c(12022,22022,32022,42022))
+#nba_data <- nba_data %>%
+#  mutate(wl_home = ifelse(wl_home == "W", 1, 0))
+#nba_data <- nba_data %>%
+#  select(wl_home, team_name_home, team_name_away)
+
+#gamlss_model2 <- gamlss(
+#  wl_home ~ team_name_home + team_name_away,
+#  family = BI,  # Binomial distribution
+#  data = nba_data
+#)
+
+
+#new_game2 <- data.frame(
+#  team_name_home = "Denver Nuggets",
+#  team_name_away = "Brooklyn Nets"
+#)
+
+#predicted_prob2 <- predict(gamlss_model2, newdata = new_game2, type = "response")
+
+#predicted_prob2
+
+
+
 
 
 # Function to fetch odds from the Odds API
@@ -71,7 +109,8 @@ odds <- get_odds(api_key, sport)
 
 if ("bookmakers" %in% names(odds)) {
   # Access and print bookmakers' odds for this event
-  for (bookmaker in odds$bookmakers) {
+  for (j in seq_along(odds$bookmakers)) {
+    bookmaker <- odds$bookmakers[[j]] 
     
     # Loop through each market for the bookmaker
     for (i in seq_along(bookmaker$markets)) {
@@ -83,11 +122,27 @@ if ("bookmakers" %in% names(odds)) {
       # Loop through each outcome within the market
       for (outcome in market$outcomes) {
         for (outcome in market$outcomes) {
-          cat("Team/Outcome:", outcome$name, "\n")
-          cat("Odds:", outcome$price, "\n")
+          
+          new_game <- data.frame(
+            Home = odds$home_team[j],
+            Visitor = odds$away_team[j]
+          )
+          predicted_prob <- predict(gamlss_model, newdata = new_game, type = "response")
+          cat(odds$home_team[j]," have ", predicted_prob*100 ,"% chance to beat ",odds$away_team[j],"\n" )
           
           # Print the calculated price as 1/price * 100
-          cat("Calculated Price:", 1 / outcome$price * 100, "\n\n")
+          if( 1 / outcome$price[outcome$name == odds$home_team[j]] < predicted_prob){
+            cat(green("Betting odds Home Team ",odds$home_team[j]," : ", 1 / outcome$price[outcome$name == odds$home_team[j]] * 100, "\n"))
+          }
+          else{
+            cat(red("Betting odds Home Team ",odds$home_team[j]," : ", 1 / outcome$price[outcome$name == odds$home_team[j]] * 100, "\n"))
+          }
+          if( (1 / outcome$price[outcome$name == odds$away_team[j]]) < (1-predicted_prob)){
+            cat(green("Betting odds Away Team ",odds$away_team[j]," : ", 1 / outcome$price[outcome$name == odds$away_team[j]] * 100, "\n\n"))
+          }
+          else{
+            cat(red("Betting odds Away Team ",odds$away_team[j]," : ", 1 / outcome$price[outcome$name == odds$away_team[j]] * 100, "\n\n"))
+          }
         }
       }
     }
